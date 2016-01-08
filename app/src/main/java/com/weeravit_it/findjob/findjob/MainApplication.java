cad982b2cc81a874cf2b560a08ad78a6dd70c0d8
Weeravit Sirita <weeravit.sirita@gmail.com>
2016-01-08 18:00:33 +0700

    update project

--- app/src/main/java/com/weeravit_it/findjob/findjob/MainApplication.java
+++ app/src/main/java/com/weeravit_it/findjob/findjob/MainApplication.java
@@ -4,15 +4,12 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
-import com.crashlytics.android.Crashlytics;
 import com.joanzapata.iconify.Iconify;
 import com.joanzapata.iconify.fonts.FontAwesomeModule;
 import com.mobsandgeeks.saripaar.Validator;
 import com.weeravit_it.findjob.findjob.utils.Contextor;
 import com.weeravit_it.findjob.findjob.utils.validates.Age;
 import com.weeravit_it.findjob.findjob.utils.validates.PhoneThai;
-
-import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Weeravit on 20/8/2558.
@@ -22,7 +19,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
+//        Fabric.with(this, new Crashlytics());
 
         Iconify.with(new FontAwesomeModule());
 //                .with(new MaterialModule());
